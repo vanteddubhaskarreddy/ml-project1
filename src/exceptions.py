97bbe7a0c3bb3e83073bfb961580e169e35107b5
vanteddubhaskarreddy <vanteddubhaskarreddy@gmail.com>
2024-03-02 19:27:44 -0600

    Added folders components, pipeline and files exceptions,logger,utils

---
+++ src/exceptions.py
@@ -0,0 +1,36 @@
+import sys
+import logging
+import logger
+
+def error_message_detail(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = 'Error in python script :[{0}] \t on line number : [{1}] \t with error message : [{2}]'.format(file_name,exc_tb.tb_lineno,str(error) )
+    return error_message
+    # This function will print the error message with the file name and line number where the error occured.
+    #_,_,exc_tb = error_detail.exc_info() will get the file name and line number where the error occured.
+    #The first two arguments are the type of exception and the value of the exception. The third argument is the traceback.
+    #exc_tb.tb_frame.f_code.co_filename here order is traceback frame,frame code,code filename will get the file name where the error occured.
+    #the parameter of function error_message_detail is error and error_detail:sys. error is the error message and error_detail:sys is the traceback of the error.
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail = error_detail)
+    
+    def __str__(self):
+        return self.error_message
+
+    #This class will create a custom exception with the error message and error detail inherited from the Exception class.
+    #The __init__ function will initialize the error message and error detail.
+    #The super.__init__(error_message) will call the parent class constructor and initialize the error message.
+    #super().__init__(error_message) This line calls the constructor of the base class (Exception). This is necessary because CustomException is a subclass of Exception, and we want to make sure that any initialization that Exception does is also done for CustomException. The error_message is passed to the Exception constructor, which uses it to set the error message for the exception.
+    #The error_message_detail function will return the error message with the file name and line number where the error occured.
+    #The __str__ function will return the error message.
+
+if __name__ == '__main__':
+    try:
+        a = 1/0
+    except Exception as e:
+        logger.logging.info('Divide by zero error')
+        raise CustomException(e,sys)
---
+++ src/logger.py
@@ -0,0 +1,32 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+#This line will create a log file with the current date and time.
+#datetime.now().shrftime('%m_%d_%Y_%H_%M_%S') will get the current date and time and convert it into the string format.
+#f"{datetime.now().shrftime('%m_%d_%Y_%H_%M_%S')}.log" will create a log file with the current date and time.
+
+logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
+#This line will create a log file with the current date and time in the logs folder.
+#os.path.join(os.getcwd(), 'logs', LOG_FILE) will join the current working directory, logs folder and log file name.
+
+os.makedirs(logs_path, exist_ok = True)
+#This line will create a directory with the log file name and exist_ok = True will not raise an error if the directory already exists.
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+#This line will join the log file path with the log file name and create a log file.
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    format = '[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO,
+)
+#This line will configure the logging module with the log file path, format, and level.
+#filename = LOG_FILE_PATH will set the log file path.
+#format = '%(asctime)s - %(lineno)s - %(name)s - %(levelname)s - %(message)s' will set the format of the log message with the date and time, line number, name, level, and message.
+#level = logging.INFO will set the level of the log message to INFO which means all the messages with level INFO or higher will be logged.
+
+
+if __name__ == '__main__':
+    logging.info('This is a test message for the logger')
